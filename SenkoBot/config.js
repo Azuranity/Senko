347e17cfe2aae06ed444768de9b985038b4a65c9
Azur <ardatr6446@gmail.com>
2025-01-23 13:14:38 +0300

    Add files via upload

---
+++ SenkoBot/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    prefix: "!",
+    botdavet: "https://discord.com/oauth2/authorize?client_id=1331331884533158021&permissions=8&integration_type=0&scope=bot",
+    desteksunucusu: "https://discord.gg/5bCcKTkmwn",
+    website: "https://google.com",
+    topgg: "https://google.com",
+    politika: "https://google.com",
+    sunucuid: "1290119764575784973",
+    destekçi: "1290126017134067864"
+}
+
